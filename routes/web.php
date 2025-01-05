46c37e9274e6a4b4414bcd8920d964026435f09f
hieuchance2018 <hieuchance2018@gmail.com>
2025-01-05 12:52:14 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,13 @@ use App\Http\Controllers\Backend\OrderController;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    // return Inertia::render('Welcome', [
+    //     'canLogin' => Route::has('login'),
+    //     'canRegister' => Route::has('register'),
+    //     'laravelVersion' => Application::VERSION,
+    //     'phpVersion' => PHP_VERSION,
+    // ]);
+    return redirect('/login');
 });
 
 Route::get('/dashboard', function () {
